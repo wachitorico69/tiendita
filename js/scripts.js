23ae9ac03d4d8dea6496fc0c55fef59b012a1f76
wachitorico69 <morenodario919@gmail.com>
2025-02-22 12:31:41 -0600

    parece q funciona la tabla

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+//Recupera películas de localStorage o inicializa un array vacío
+let movies = JSON.parse(localStorage.getItem('movies')) || [];
+
+const movieTableBody = document.querySelector('#movieTable tbody'); //la tabla que tenemos en html
+let editingIndex = null;
+
+//Función para cargar las películas en la tabla
+function renderMovies() {
+    movieTableBody.innerHTML = ''; //limpia la tabla
+    movies.forEach((movie, index) => {  //recorre el array de libros
+        const row = document.createElement('tr'); //se crea un nuevo renglón en la tabla y en cada una de las 4 celdas los datos de los libros
+        row.innerHTML = `
+            <td>${movie.producto}</td>  
+            <td>${movie.precio}</td>
+            <td>
+                <button onclick="deleteMovie(${index})"><img src="/media/delete.png"width="30px"></button>
+            </td>
+        `;
+        movieTableBody.appendChild(row); //se agrega el renglón que creamos con datos a la tabla
+    });
+}
+
+function añadir(producto, precio) {
+    const newMovie =  {producto, precio};
+
+    movies.push(newMovie);
+
+    localStorage.setItem('movies', JSON.stringify(movies));
+
+    renderMovies();
+}
+
+function deleteMovie(index) {
+    books.splice(index,1); //tabla
+    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.removeItem(JSON.stringify(books[index])); //borra localstorage
+    renderBooks();
+}
+
+//Render inicial, llama a la función anterior
+renderMovies();
